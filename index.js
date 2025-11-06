cdeca47b0bbd0d0e5c8fdced1a94aa32aee2e5d4
Masoud <111156543+MrCarbone@users.noreply.github.com>
2025-11-07 01:19:15 +0330

    Update index.js

--- index.js
+++ index.js
@@ -3,20 +3,23 @@ import {readFileSync,existsSync} from 'node:fs';
 import { join } from 'path';
 const serve = new Server();
 serve.on('request', (req, res) => {
-  if(!existsSync(j('public',req.url)) {
+
+  console.log(req.url)
+  if(!existsSync(j('public',home(req.url)))) {
     res.end('');
     return;
   }
   res.writeHead(200, {
    'content-type': mime(req.url) || 'text/plain'
   });
-  res.end(readFileSync(j('public',req.url), 'utf8'));
+  
+  res.end(readFileSync(j('public',home(req.url)), 'utf8'));
 });
 serve.listen(8000);
 
-funcion mime(a) {
+function mime(a) {
   return {
-    '/public/style.css': 'text/css',
+    '/style.css': 'text/css',
     '/': 'text/html;charset=utf-8',
     '/favicon.ico': 'image/x-icon'
   }[a];
@@ -26,17 +29,6 @@ function j(...a) {
   return join.apply(null,[process.cwd(),...a])
 }
 
-/*import express from 'express';
-const app = express();
-
-app.get('/', (req,res) => {
-  res.send('hello');
-});
-app.get('/api', async (req,res) => {
-  const request = await fetch('https://time.ir');
-  const response = await request.text();
-  res.send(response);
-});
-
-app.listen(8000);
-*/
+function home(a) {
+  return a==='/'?'/index.html':a;
+}
