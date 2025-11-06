92959b5a045f3e55eeaae73d1396987fabf7505f
Masoud <111156543+MrCarbone@users.noreply.github.com>
2025-11-06 16:14:17 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import express from 'express';
 const app = express();
+
 app.get('/', (req,res) => {
   res.send('hello');
 });
+app.get('/api', (req,res) => {
+  res.send('firt api');
+});
+
 app.listen(8000);
