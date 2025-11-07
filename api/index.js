b0854ece0adb6dabd67215e41153d3f99ae16672
Masoud <111156543+MrCarbone@users.noreply.github.com>
2025-11-07 12:46:29 +0330

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,9 @@
 export default {
   fetch(request: Request) {
+    const url = new URL(request.url):
+    if(url.pathname === 'a') {
+      return new Response('Hello from Vercel!');
+    }
     return new Response('Hello from Vercel!');
   },
 };
